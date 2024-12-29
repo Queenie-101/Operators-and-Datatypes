b77cb15410ca6be9d7b097d91943bf4f2894f2c5
Esther <queenestheradekanm@gmail.com>
2024-12-29 19:56:22 +0100

    I learnt about basic operators and datatypes in python.

---
+++ main.py
@@ -0,0 +1,10 @@
+name = "Danielle"
+age = 17
+is_student = True
+weight = 57.8
+print("The data type of name is:", type(name))
+print("The data type of age is:", type(age))
+print("The data type of is_student is:", type(is_student))
+print("The data type of weight is:", type(weight))
+print("The new data type of age is: ",str(type(age)))
+print("The new data type of name is: ",int(type(name)))
